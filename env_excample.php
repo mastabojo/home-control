af24ed13c83b203cdeec90ac498e6dd9d6b3432d
Bojo <bojo.recko@yahoo.com>
2019-02-03 11:58:08 +0100

    Added env_example.php

---
+++ env_excample.php
@@ -0,0 +1,25 @@
+<?php
+$APPLICATION_NAME = 'Home control center';
+$LANGUAGE = 'sl';
+$PRODUCTION = false;
+
+### Database
+$DB_HOST = 'localhost';
+$DB_NAME = '';
+$DB_USER = '';
+$DB_PASS = '';
+
+# Weather data provider
+# - OWM (openWetherMap)
+#   $WEATHER_PROVIDER_API_KEY = ';
+#   $WEATHER_PROVIDER_CITY_ID = <CITY_ID>
+
+### $WEATHER_PROVIDER = 'ARSO';
+$WEATHER_PROVIDER = 'OWM';
+$WEATHER_PROVIDER_API_KEY = '';
+$WEATHER_PROVIDER_CITY_ID = 3196359; // OWM city ID: Ljubljana
+
+### Open Weather Map related data
+$OPENWEATHERMAP_API_KEY = '';
+$OPENWEATHERMAP_CITY_ID = 3196359; // OWM city ID: Ljubljana
+
