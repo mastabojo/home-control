ca455792297169d285ac3e61548548e40c28b3c5
Bojo <bojo.recko@yahoo.com>
2019-02-09 22:50:22 +0100

    Fixed a typo in var name

--- jobs/overheat_control.py
+++ jobs/overheat_control.py
@@ -1,7 +1,7 @@
 import os
 
 criticalTemp = 7500
-tmpInfo = open("/sys/class/thermal/thermal_zone0/temp", "r")
+tempInfo = open("/sys/class/thermal/thermal_zone0/temp", "r")
 cpuTemp = tempInfo.read()
 if cpuTemp > criticalTemp:
     os.system() 
