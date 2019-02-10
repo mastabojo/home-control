5fdab9d7fdf2e14bc67680e660d5b1476263d6d4
Bojo <bojo.recko@yahoo.com>
2019-02-10 15:53:25 +0100

    Fixed a couple of bugs

--- jobs/cpu_temperature_log.php
+++ jobs/cpu_temperature_log.php
@@ -1,8 +1,8 @@
 <?php
-include '../env.php';
+include_once dirname(__DIR__) . '/env.php';
 
 $cpuTemperatureInfoPath = '/sys/class/thermal/thermal_zone0/temp';
 $cpuTemperature = file($cpuTemperatureInfoPath);
 
-echo "TEMPERATURE: $temperature";
+echo "TEMPERATURE: $cpuTemperature\n";
 
