4b1800548322e9c7670ff5fb28bd427f745c38bc
Bojo <bojo.recko@yahoo.com>
2019-02-24 21:09:48 +0100

    Fixed command for starting the main Python loop

--- bootstrap/hcc_init.sh
+++ bootstrap/hcc_init.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # hcc_init.sh
+# Starts from cron
 # Does some initial tasks on boot
 
 # Start the main loop for Python scripts
 cd /
 cd /var/www/hcc.local/py
-/usr/bin/python /var/www/hcc.local/py/mainloop.py
+/usr/bin/python /var/www/hcc.local/py/mainloop.py &
 cd /
