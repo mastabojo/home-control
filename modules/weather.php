2e76edbbd1d32c5b00a066781271001db46a9a7c
Bojo <bojo.recko@yahoo.com>
2019-03-10 19:33:20 +0100

    Added Windy weather iframe on the weather pane

--- modules/weather.php
+++ modules/weather.php
@@ -1,3 +1,38 @@
 <?php
+$theme = 'dark';
+$windyUrl = "https://embed.windy.com/embed2.html
+?lat=46.048
+&lon=14.505
+&zoom=11
+&level=surface
+&overlay=temp
+&menu=
+&message=true
+&marker=
+&calendar=
+&pressure=
+&type=map
+&location=coordinates
+&detail=
+&detailLat=50.090
+&detailLon=14.420
+&metricWind=m%2Fs
+&metricTemp=%C2%B0C
+&radarRange=-1";
 ?>
-<h1>Weather</h1>
+
+<div class="row">
+
+<div class="col">
+
+<img src="/img/weather-icons/windy-weather.svg" style="width:60px;">
+
+</div><!-- .col -->
+
+<div class="col">
+<iframe width="680" height="374" src="<?php echo $windyUrl;?>" frameborder="0"></iframe> 
+</div><!-- .col -->
+
+</div><!-- .row -->
+
+
