f011b9d8d7d02bdef0b4f706a00489c91d637b66
Bojo <bojo.recko@yahoo.com>
2019-03-14 20:01:32 +0100

    Fixed system comands script

--- public/api/system_commands.php
+++ public/api/system_commands.php
@@ -1,13 +1,15 @@
 <?php
 $allowedCommands = [
-    'exit-browser' => 'pkill chromium',
-    'reboot' => 'reboot now',
-    'shutdown' => 'halt',
+    'exit-browser' => 'sudo pkill chromium',
+    'reboot' => 'sudo reboot now',
+    'shutdown' => 'sudo halt',
 ];
 
 // get rid of all unexpected stuff
 if(!isset($_POST['cmd']) || empty($_POST['cmd']) || !array_key_exists($_POST['cmd'], $allowedCommands)) {
     exit();
 }
+// log action
+error_log("System command executed ({$allowedCommands[$_POST['cmd']]})");
 // execute command
 exec('sudo ' . $allowedCommands[$_POST['cmd']]);
