237eeda94f628bbbb5f7eec7dce0f89b14481503
Bojo <bojo.recko@yahoo.com>
2019-05-11 22:24:46 +0200

    Changed api test

--- public/api/api.php
+++ public/api/api.php
@@ -3,9 +3,9 @@
 
 /**/
 // Test holidays
-include_once dirname(__DIR__) . '/api/class.CalendarHolidays.php';
-$c = new CalendarHolidays();
-print_r($c->getHolidayDates());
+// include_once dirname(__DIR__) . '/api/class.CalendarHolidays.php';
+// $c = new CalendarHolidays();
+// print_r($c->getHolidayDates());
 
 /*
 // Test garbage collection stuff
@@ -15,16 +15,15 @@ print_r($g->getAllDates());
 // print_r($g->getDatesForType('bio'));
 */
 
-/*
+/**/
 // Test weather class
 // ------------------
 // include_once dirname(__DIR__) . '/api/class.WeatherOWM.php';
 include_once dirname(__DIR__) . '/api/class.WeatherARSO.php';
 // $Weather = new WeatherOWM;
 $Weather = new WeatherARSO;
-$weather = $Weather->getWeatherCurrentDigest();
-// $weather = $Weather->getWeatherForecastDigest();
+// $weather = $Weather->getWeatherCurrentDigest();
+$weather = $Weather->getWeatherForecastDigest();
 echo "\n\n";
 print_r($weather);
 echo "\n\n";
-*/
