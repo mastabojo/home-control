b3819539f2326a72de5278ebd47b3dbe4fcccc1c
Bojo <bojo.recko@yahoo.com>
2019-12-24 12:24:32 +0100

    Fixed bug in managing relay with curl

--- public/api/handleHttpSwitches.php
+++ public/api/handleHttpSwitches.php
@@ -36,7 +36,7 @@ curl_setopt_array($curl, [
     CURLOPT_POSTFIELDS => ['id' => '']
 ]);
 $response = curl_exec($curl);   
-curl_close();
+curl_close($curl);
 
 $allowedResponses = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
 echo in_array($response, $allowedResponses) ? $response : '0';  
