b1222308d09cdf0e422b6f3d8449e5babf831678
Bojo <bojo.recko@yahoo.com>
2020-12-06 14:59:25 +0100

    Working on MQTT subscribe PHP script

--- public/api/subscribeMQTT.php
+++ public/api/subscribeMQTT.php
@@ -60,6 +60,8 @@ function subscribe() {
  */
 function message($message) {
 
+    logError('MESSAGE: ' . message);
+
     global $DB_HOST, $DB_NAME, $DB_USER, $DB_PASS;
 
     $topicArr = explode('/', $message->topic);
@@ -69,41 +71,53 @@ function message($message) {
         exit();
     }
 
-    // group and entity from the topic structure for writing into database
-
-    $entity = $topicArr[2];
-
     $DB = getDB($DB_HOST, $DB_NAME, $DB_USER, $DB_PASS);
 
-    // Check if the entry for this entity (param_name) exists in the table
-    $q = "SELECT id FROM app_state WHERE param_name=:entity";
-    $qp = [':entity' => $entity];
-    $stmt = $DB->prepare($q);
-    try {
-        $stmt->execute($qp);
-        $res = $stmt->fetch(PDO::FETCH_NUM);
-    } catch (PDOException $e) {
-        logError($errorMsg['dbselect'] . ' (' . $e->getMessage() . ')');
-        exit();
-    }
-    
-    // Table entry for the entity (parameter_name) exists - do UPDATE
-    if(isset($res) && isset($res[0])) {
-        $entityId = $res[0];
-        $q = "UPDATE app_state SET param_value=:paramvalue WHERE param_name=:paramname;";
-        $qp = [':paramvalue' => $message->payload, ':paramname' => $entity];
-    } 
-    
-    // Table entry for the entity (parameter_name) does not exist - do INSERT
-    else {
-        $q = "INSERT INTO app_state (param_name, param_value, param_comment) VALUES (:paramname, :paramvalue, 'Inserted thru MQTT message');";
-        $qp = [':paramvalue' => $message->payload, ':paramname' => $entity];
-    }
+    // If topic is home/booted/... this is a signal, that device was (re)booted and should have settings restored from the database
+    if($topicArr[1] == 'booted') {
+        $entity = $topicArr[3];
+        $publishTopic = $topicArr[0] . '/' . $topicArr[2] . '/' . $topicArr[3];
 
-    // Get the query do its work
-    $stmt = $DB->prepare($q);
-    $stmt->execute($qp);
+        // Code for reading settings from database and publishing to the switch
+
+    // If regular topic 
+    } else {
+
+        // group and entity from the topic structure for writing into database
+        $entity = $topicArr[2];
+
+        // Check if the entry for this entity (param_name) exists in the table
+        $q = "SELECT id FROM app_state WHERE param_name=:entity";
+        $qp = [':entity' => $entity];
+        $stmt = $DB->prepare($q);
+        try {
+            $stmt->execute($qp);
+            $res = $stmt->fetch(PDO::FETCH_NUM);
+        } catch (PDOException $e) {
+            logError($errorMsg['dbselect'] . ' (' . $e->getMessage() . ')');
+            exit();
+        }
+        
+        // Table entry for the entity (parameter_name) exists - do UPDATE
+        if(isset($res) && isset($res[0])) {
+            $entityId = $res[0];
+            $q = "UPDATE app_state SET param_value=:paramvalue WHERE param_name=:paramname;";
+            $qp = [':paramvalue' => $message->payload, ':paramname' => $entity];
+        } 
+        
+        // Table entry for the entity (parameter_name) does not exist - do INSERT
+        else {
+            $q = "INSERT INTO app_state (param_name, param_value, param_comment) VALUES (:paramname, :paramvalue, 'Inserted thru MQTT message');";
+            $qp = [':paramvalue' => $message->payload, ':paramname' => $entity];
+        }
+
+        // Get the query do its work
+        $stmt = $DB->prepare($q);
+        $stmt->execute($qp);
+    }        
 }
+
+
 
 function disconnect() {
     logEvent("MQTT client disconnected");
